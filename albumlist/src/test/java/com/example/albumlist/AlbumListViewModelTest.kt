62b2b6497eda1c5e62d14da08fc42a4695d23c7d
adam.fejes <fejes91@gmai.com>
2026-01-31 13:03:01 +0100

    test: add basic tests to album list viewmodel

---
+++ albumlist/src/test/java/com/example/albumlist/AlbumListViewModelTest.kt
@@ -0,0 +1,62 @@
+package com.example.albumlist
+
+import app.cash.turbine.test
+import com.example.albumlist.model.AlbumListViewModel
+import com.example.domain.model.Album
+import com.example.domain.repository.AlbumRepository
+import com.example.navigation.Navigator
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+internal class AlbumListViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @MockK
+    lateinit var navigator: Navigator
+
+    @MockK
+    lateinit var albumRepository: AlbumRepository
+
+    @Before
+    fun setUp() = MockKAnnotations.init(this, relaxUnitFun = true)
+
+    @Test
+    fun viewModelCallsRepositoryOnCreation() = runTest {
+        createSut()
+
+        coEvery { albumRepository.fetchAlbumList() }
+    }
+
+    @Test
+    fun viewModelGetsAlbumData() = runTest {
+        every { albumRepository.getAlbumList() } returns flowOf(listOf(mockk<Album>(relaxed = true) {
+            every { title } returns "title"
+        }))
+
+        val sut = createSut()
+
+        sut.albums.test {
+            val result = awaitItem()
+
+            assertEquals(result.size, 1)
+            assertEquals(result.first().title, "title")
+        }
+    }
+
+    private fun createSut() = AlbumListViewModel(navigator, albumRepository)
+}
